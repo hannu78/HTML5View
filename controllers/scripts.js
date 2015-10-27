966c58b72c1692bf2d691c3ba7c103b6768f2507
hannu78 <hannu.lisko@iki.fi>
2015-10-27 13:10:14 +0200

    Tiistain muutokset

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,1 +1,26 @@
 console.log("Here we go!!");
+
+window.onload = function(event) {
+    //"use strict";
+    console.log(event);
+    para1.innerHTML = "Changed from JS";
+    para1.style.backgroundColor = "blue";
+    var temp = document.createElement("p");
+    temp.innerHTML = "New element";
+    para1.appendChild(temp);
+};
+
+function domReady(event) {
+    "use strict";
+    return 2;
+}
+
+//window.onload = domReady;
+
+function someFunction(nimi) {
+    "use strict";
+    console.log(nimi);
+}
+
+someFunction();
+someFunction("Hannu");
